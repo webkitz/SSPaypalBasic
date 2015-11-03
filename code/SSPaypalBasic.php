fcc844ca16a7cb10e900f68d61eab222878c695c
Luke Hardiman <luke@hardiman.co.nz>
2015-11-03 22:10:41 +1300

    Skeleton Structure of module -skeleton structure of module

--- code/SSPaypalBasic.php
+++ code/SSPaypalBasic.php
@@ -3,7 +3,12 @@
 class SSPaypalBasic extends DataExtension
 {    
     protected static $sandBox = false;
-
+    protected static $_items = array();
+    protected static $payPalData = array(
+        'username'  =>          '',
+        'password'  =>          '',
+        'signature' =>          ''
+    );
 
     /**
      * @param $sandbox bool sets sandbox mode
@@ -12,5 +17,25 @@ class SSPaypalBasic extends DataExtension
     {
         self::$sandBox = true;
     }
+
+    /**
+     * @param array $payPalApi details
+     */
+    public static function setPaypal($payPalApi = array()){}
+
+    /**
+     * @param array $cartItem | Add item to cart
+     */
+    public static function addCart($cartItem = array()){}
+
+    /**
+     * @param array $cartItem Remove item from cart
+     */
+    public static function removeCart($cartItem = array()){}
+
+    /**
+     * @param array $cartItem | Emptyies cart
+     */
+    public static function emptyCart($cartItem = array()){}
 
 }
