52778f55d5a63cb9440f7924c8cc35d24ffdb818
lukesUbuntu <luke@hardiman.co.nz>
2015-11-08 16:12:20 +1300

    Updating paypal

--- code/SSPaypalBasic.php
+++ code/SSPaypalBasic.php
@@ -4,6 +4,7 @@ class SSPaypalBasic extends DataExtension
 {    
     protected static $_sandBox = false;
     protected static $_items = array();
+    protected static $_cartID = 0;
     protected static $payPalData = array(
         'username'  =>          '',
         'password'  =>          '',
@@ -19,18 +20,25 @@ class SSPaypalBasic extends DataExtension
     }
 
     /**
+     * Update the cart ID
+     */
+    public static function cartId()
+    {
+        self::$_cartID++;
+    }
+    /**
      * @param array $payPalApi details
      */
     public static function setPaypal($payPalApi = array()){}
 
     /**
      * @param array $addCartButton | Add item to cart
-     * @return button
+     * @return String button
      */
-    public static function addCartButton($cartItem = array()){
-        $cartItem = (object)$cartItem;
-        $defaultBtn = isset($cartItem->button) ? $cartItem->button : 'Add to card';
-        return "<span class='addToCart' data-code='$cartItem->code' data-price='$cartItem->price' data-name='$cartItem->name'>$defaultBtn</span>";
+    public static function addCartButton($name = '',$price = '', $code = false, $button = 'Add to card'){
+
+        $code = ($code == false)?  self::cartId() : $code;
+        return "<span class='addToCart' data-code='$code' data-price='$price' data-name='$name'>$button</span>";
     }
 
     /**
